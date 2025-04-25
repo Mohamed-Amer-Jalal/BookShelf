9d18a0a973673b857d684528ced18cb9e21f9ac6
M-Amer <mohamedamerjalal@gmail.com>
2025-04-26 02:45:04 +0300

    Refactor: Implement book favoriting and update QueryUiState and QueryViewModel
    - Changed `QueryUiState.Success` to hold a list of `Book` instead of a String.
    - Added logic to `QueryViewModel` for managing favorite books:
      - Added `_favoriteBooks` and `favoriteBooks` to store favorite `Book`s.
      - Added `_favoritesUiState` and `favoritesUiState` to track the state of the favorites.
      - Added `isFavoriteBook`, `addFavoriteBook`, and `removeFavoriteBook` functions to manage the favorite list.
      - Added `favoritesUpdated` function to update the `favoritesUiState` when the list changes.
    - Removed unused `uiState` and its references.
    - Added `getBooks` function to start search.
    - Renamed `updateSearchStarted` to private.

--- app/src/main/java/com/example/bookshelf/screens/queryScreen/QueryUiState.kt
+++ app/src/main/java/com/example/bookshelf/screens/queryScreen/QueryUiState.kt
@@ -1,7 +1,9 @@
 package com.example.bookshelf.screens.queryScreen
 
+import com.example.bookshelf.model.Book
+
 sealed interface QueryUiState {
-    data class Success(val bookshelfList: String) : QueryUiState
+    data class Success(val bookshelfList: List<Book>) : QueryUiState
     object Error : QueryUiState
     object Loading : QueryUiState
 }
--- app/src/main/java/com/example/bookshelf/screens/queryScreen/QueryViewModel.kt
+++ app/src/main/java/com/example/bookshelf/screens/queryScreen/QueryViewModel.kt
@@ -1,22 +1,67 @@
 package com.example.bookshelf.screens.queryScreen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bookshelf.model.Book
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class QueryViewModel : ViewModel() {
-    /*private val _uiState = MutableStateFlow<QueryUiState>(QueryUiState.Loading)
-    val uiState: StateFlow<QueryUiState> = _uiState.asStateFlow()*/
+    private val _uiState = MutableStateFlow<QueryUiState>(QueryUiState.Loading)
+    val uiState: StateFlow<QueryUiState> = _uiState.asStateFlow()
 
     private val _uiStateSearch = MutableStateFlow(SearchUiState())
     val uiStateSearch: StateFlow<SearchUiState> = _uiStateSearch.asStateFlow()
+
+    // Logic for Favorite books -- Beg
+    private val _favoriteBooks = mutableStateListOf<Book>()
+    val favoriteBooks: List<Book> = _favoriteBooks
+
+    private var _favoritesUiState: QueryUiState by mutableStateOf(QueryUiState.Loading)
+    val favoritesUiState: QueryUiState = _favoritesUiState
+
+    fun isFavoriteBook(book: Book): Boolean {
+        return !_favoriteBooks.none { it.id == book.id }
+    }
+
+    fun addFavoriteBook(book: Book) {
+        if (!isFavoriteBook(book)) {
+            _favoriteBooks.add(book)
+            favoritesUpdated()
+        }
+    }
+
+    fun removeFavoriteBook(book: Book) {
+        _favoriteBooks.removeIf { it.id == book.id }
+        favoritesUpdated()
+    }
+
+    private fun favoritesUpdated() {
+        viewModelScope.launch {
+            _favoritesUiState = QueryUiState.Loading
+            _favoritesUiState = QueryUiState.Success(favoriteBooks)
+        }
+    }
+    // Logic for Favorite books -- End
 
     fun updateQuery(query: String) {
         _uiStateSearch.value = _uiStateSearch.value.copy(query = query)
     }
 
-    fun updateSearchStarted(searchStarted: Boolean) {
+    private fun updateSearchStarted(searchStarted: Boolean) {
         _uiStateSearch.value = _uiStateSearch.value.copy(searchStarted = searchStarted)
+    }
+
+    fun getBooks(query: String = "") {
+        updateSearchStarted(true)
+        viewModelScope.launch {
+
+        }
     }
 }
